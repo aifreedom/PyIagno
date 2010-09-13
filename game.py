d611f284b7f2ea88d4db0f7106710f453f59505b
aifreedom <me@aifreedom.com>
2010-09-13 17:50:54 +0800

    First commit

---
+++ game.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python
+
+# PyIagno - wxPython based implementation of a classial Reversi game
+#
+# Copyright (C) 2010 aifreedom <me@aifreedom.com>
+#
+# PyIagno is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# PyIagno is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyIagno; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+import copy
+
+class IagnoGame(object):
+    """
+    Implements the Reversi game.
+    """
+    
+    # Enumerator of colors on board
+    BRD_BLANK, BRD_DARK, BRD_LIGHT = range(-1, 2)
+
+    # Directions to move
+    __dir = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1),
+             (1, -1), (1, 0), (1, 1))
+
+    # =======================================================
+    # Exceptions
+    # =======================================================
+    class NotFoundException(Exception): pass
+    class InvalidPositionException(Exception): pass
+
+    # =======================================================
+    # Constructor
+    # =======================================================
+    def __init__(self, init=None):
+        """
+        Initialize a new game.
+        init: the initial outline of the game.
+        """
+        self.__Board = []              # set the initial board
+        if init != None:               # if init is provided
+            for i in init:
+                self.__Board.append(i[:])
+        else:
+            for i in range(8):
+                self.__Board.append([self.BRD_BLANK] * 8)
+            self.__Board[3][4] = self.BRD_DARK
+            self.__Board[4][3] = self.BRD_DARK
+            self.__Board[3][3] = self.BRD_LIGHT
+            self.__Board[4][4] = self.BRD_LIGHT
+
+        self.__Len = 0                 # steps been taken
+
+        self.__Valid = []              # valid place to set
+        self.__Valid.append([])
+        self.__Valid.append([])
+        
+        self.__Player = self.BRD_DARK  # initial player: Dark
+        self.__End = False
+
+        self.__Maintain()
+
+    # =======================================================
+    # Public methods
+    # =======================================================
+    def Set(self, pos):
+        """
+        Sets a piece at pos for player.
+        Returns the changed grids.
+        If pos is not valid for player, raises InvalidPositionException.
+        """
+        player = self.__Player
+        if pos in self.GetValid(player):
+            x, y = pos
+            self.__Board[x][y] = player
+            self.__Len += 1
+            self.__Player = not self.__Player
+            return self.__Update(pos)
+        else:
+            raise self.InvalidPositionException
+        
+    def GetValid(self, player):
+        """
+        Returns the valid posiitions for player.
+        """
+        return tuple(self.__Valid[player])
+
+    def GetPlayer(self):
+        """
+        Returns the color of the current player.
+        """
+        return self.__Player
+
+    def IsEnd(self):
+        """
+        Returns if the game is end.
+        """
+        return self.__End
+
+    # =======================================================
+    # Overloaded methods
+    # =======================================================
+    def __len__(self):
+        """
+        The length of a game is the current number of steps
+        """
+        return self.__Len
+
+    def __getitem__(self, idx):
+        """
+        Access to the board by index
+        """
+        return self.__Board[idx][:]
+
+
+    # =======================================================
+    # Private methods
+    # =======================================================
+    def __Update(self, pos):
+        """
+        Update the board, after put a piece on pos.
+        Returns a tuple containing the positions that was updated.
+        """
+        x, y = pos
+        player = self[x][y]
+        ret = [pos]
+        for d in self.__dir:
+            try:
+                li = self.__Find(player, pos, d)
+            except:
+                pass
+            else:
+                for x, y in li:
+                    self.__Board[x][y] = not self.__Board[x][y]
+                ret += li
+        self.__Maintain()
+        return tuple(ret)
+
+    def __Find(self, color, pos, d):
+        """
+        Find the consecutive grids which have different color with the color of pos in direction d.
+        Returns the tuple of founded positons.
+        If not found, raises NotFoundException.
+        """
+        x, y = pos
+        dx, dy = d
+        ret = []
+        x += dx
+        y += dy
+        while self[x][y] != color:
+            if (not (x in range(8) and y in range(8))
+                or self[x][y] == self.BRD_BLANK):
+                break
+            ret.append((x, y))
+            x += dx
+            y += dy
+        else:
+            if ret != []:
+                return tuple(ret)
+        raise self.NotFoundException
+
+    def __Maintain(self):
+        """
+        Maintains the valid positions for each player.
+        """
+        self.__Valid[0] = []
+        self.__Valid[1] = []
+        for pos in [(x, y) for x in range(8) for y in range(8)]:
+            x, y = pos
+            if self[x][y] == self.BRD_BLANK:
+                for color in range(2):
+                    for d in self.__dir:
+                        try:
+                            self.__Find(color, pos, d)
+                        except:
+                            pass
+                        else:
+                            self.__Valid[color].append(pos)
+                            break
+        if self.__Valid[0] == [] and self.__Valid[1] == []:
+            self.__End = True
+
+    # =======================================================
+    # For debug only
+    # =======================================================
+    def __Output(self, msg=''):
+        """
+        For debug only, print the current board.
+        """
+        print 'PyIagno Debug:'
+        print msg
+        for i in self.__Board:
+            for j in i:
+                print j,
+            print
+
+# end of class IagnoGame
---
+++ test/gametest.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python
+
+# PyIagno - wxPython based implementation of a classial Reversi game
+#
+# Copyright (C) 2010 aifreedom <me@aifreedom.com>
+#
+# PyIagno is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# PyIagno is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyIagno; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+import unittest
+import sys
+sys.path.append('..')
+from game import IagnoGame
+
+
+
+class InitGameTest(unittest.TestCase):
+    def setUp(self):
+        self.Game = IagnoGame()
+
+    def testLength(self):
+        assert len(self.Game) == 0, 'length not zero'
+
+    def testInitOutline(self):
+        assert self.Game[3][3] == IagnoGame.BRD_LIGHT, '(3, 3) not light'
+        assert self.Game[4][4] == IagnoGame.BRD_LIGHT, '(4, 4) not light'
+        assert self.Game[3][4] == IagnoGame.BRD_DARK, '(3, 4) not dark'
+        assert self.Game[4][3] == IagnoGame.BRD_DARK, '(4, 3) not dark'
+
+    def testInitPlayerColor(self):
+        """Test if the first player is Dark."""
+        assert self.Game.GetPlayer() == IagnoGame.BRD_DARK
+
+    def testValidSet1(self):
+        """Set a dark piece at pos (2, 3)."""
+        self.Game.Set((2, 3))
+        spj = ((2, 3), (3, 3), (3, 4), (4, 3), (4, 4))
+        for i in range(8):
+            for j in range(8):
+                if not (i, j) in spj:
+                    assert self.Game[i][j] == IagnoGame.BRD_BLANK, \
+                           '(%d, %d) is not blank' % (i, j)
+        assert self.Game[2][3] == IagnoGame.BRD_DARK
+        assert self.Game[3][3] == IagnoGame.BRD_DARK
+        assert self.Game[4][4] == IagnoGame.BRD_LIGHT
+        assert self.Game[3][4] == IagnoGame.BRD_DARK
+        assert self.Game[4][3] == IagnoGame.BRD_DARK
+
+    def testValidSet2(self):
+        """Set a dark piece at pos (3, 2)."""
+        self.Game.Set((3, 2))
+        spj = ((3, 2), (3, 3), (3, 4), (4, 3), (4, 4))
+        for i in range(8):
+            for j in range(8):
+                if not (i, j) in spj:
+                    assert self.Game[i][j] == IagnoGame.BRD_BLANK, \
+                           '(%d, %d) is not blank' % (i, j)
+        assert self.Game[3][2] == IagnoGame.BRD_DARK
+        assert self.Game[3][3] == IagnoGame.BRD_DARK
+        assert self.Game[4][4] == IagnoGame.BRD_LIGHT
+        assert self.Game[3][4] == IagnoGame.BRD_DARK
+        assert self.Game[4][3] == IagnoGame.BRD_DARK
+
+    def testValidSet3(self):
+        """Set a dark piece at pos (4, 5)."""
+        self.Game.Set((4, 5))
+        spj = ((3, 3), (3, 4), (4, 3), (4, 4), (4, 5))
+        for i in range(8):
+            for j in range(8):
+                if not (i, j) in spj:
+                    assert self.Game[i][j] == IagnoGame.BRD_BLANK, \
+                           '(%d, %d) is not blank' % (i, j)
+        assert self.Game[4][5] == IagnoGame.BRD_DARK
+        assert self.Game[3][3] == IagnoGame.BRD_LIGHT
+        assert self.Game[4][4] == IagnoGame.BRD_DARK
+        assert self.Game[3][4] == IagnoGame.BRD_DARK
+        assert self.Game[4][3] == IagnoGame.BRD_DARK
+
+    def testValidSet4(self):
+        """Set a dark piece at pos (5, 4)."""
+        self.Game.Set((5, 4))
+        spj = ((3, 3), (3, 4), (4, 3), (4, 4), (5, 4))
+        for i in range(8):
+            for j in range(8):
+                if not (i, j) in spj:
+                    assert self.Game[i][j] == IagnoGame.BRD_BLANK, \
+                           '(%d, %d) is not blank' % (i, j)
+        assert self.Game[5][4] == IagnoGame.BRD_DARK
+        assert self.Game[3][3] == IagnoGame.BRD_LIGHT
+        assert self.Game[4][4] == IagnoGame.BRD_DARK
+        assert self.Game[3][4] == IagnoGame.BRD_DARK
+        assert self.Game[4][3] == IagnoGame.BRD_DARK
+
+
+    def testInvalidSet1(self):
+        """Set a dark piece at pos (0, 0)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (0, 0))
+        
+    def testInvalidSet2(self):
+        """Set a dark piece at pos (7, 7)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (7, 7))
+        
+    def testInvalidSet3(self):
+        """Set a dark piece at pos (0, 7)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (0, 7))
+        
+    def testInvalidSet4(self):
+        """Set a dark piece at pos (7, 0)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (7, 0))
+        
+    def testInvalidSet5(self):
+        """Set a dark piece at pos (2, 2)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (2, 2))
+
+    def testInvalidSet6(self):
+        """Set a dark piece at pos (5, 5)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (5, 5))
+
+    def testInvalidSet7(self):
+        """Set a dark piece at pos (4, 2)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (4, 2))
+
+    def testInvalidSet8(self):
+        """Set a dark piece at pos (3, 5)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (3, 5))
+
+    def testInvalidSet9(self):
+        """Set a dark piece at pos (2, 4)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (2, 4))
+
+    def testInvalidSet10(self):
+        """Set a dark piece at pos (5, 3)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (5, 3))
+
+    def testInvalidSet11(self):
+        """Set a dark piece at pos (2, 4)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (1, 3))
+
+    def testInvalidSet12(self):
+        """Set a dark piece at pos (0, 2)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (0, 2))
+
+    def testInvalidSet13(self):
+        """Set a dark piece at pos (2, 0)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (2, 0))
+
+    def testInvalidSet14(self):
+        """Set a dark piece at pos (3, 7)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (3, 7))
+
+    def testInvalidSet15(self):
+        """Set a dark piece at pos (7, 4)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (7, 4))
+
+    def testInvalidSet16(self):
+        """Set a dark piece at pos (2, 4)."""
+        self.assertRaises(IagnoGame.InvalidPositionException,
+                          self.Game.Set, (2, 4))
+
+        
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+
+    # Test initial outline
+    initSuite = unittest.makeSuite(InitGameTest, 'test')
+    suite.addTest(initSuite)
+    
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
